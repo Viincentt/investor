c5bf28b97473669b62a07b161d1d67fe34933303
Viincentt <vincent.hu@epita.fr>
2025-08-19 01:37:14 +0200

    add limit buy

--- models/alpaca.py
+++ models/alpaca.py
@@ -24,3 +24,20 @@ class Alpaca:
         response = requests.get(url, headers=self.headers)
         self.check_status(response)
         return response.json()
+
+    def limit_buy(self, ticker, amount):
+        print(f"Limit buy {amount} of {ticker}")
+        url = "https://paper-api.alpaca.markets/v2/orders"
+
+        payload = {
+            "type": "market",
+            "time_in_force": "day",
+            "symbol": ticker,
+            "notional": amount,
+            "side": "buy",
+        }
+        self.headers["content-type"] = "application/json"
+        response = requests.post(url, json=payload, headers=self.headers)
+        del self.headers["content-type"]
+        self.check_status(response)
+        return response.json()
